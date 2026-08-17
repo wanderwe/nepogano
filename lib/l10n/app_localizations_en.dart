f96dd3dda30bf89f1dd95c7fd143070579d0ab30
Ihor Allaverdiiev <allaverdiiev@gmail.com>
2026-08-17 19:41:24 +0300

    copy: revert UK nudge banner wording, shorten EN to match its tone
    "Хтось цікавиться, як ти" was better than the nudge-terminology
    swap. EN now mirrors that same "curious" framing instead of
    "nudged" - short enough to stay on one line like the UK original.

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -684,7 +684,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get nudgeListTitle => 'Who nudged you';
 
   @override
-  String get nudgeBanner => 'Someone nudged you';
+  String get nudgeBanner => 'Someone\'s curious about you';
 
   @override
   String get language => 'Language';
